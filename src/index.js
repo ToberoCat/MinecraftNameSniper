7b62205f7d966c69999ce1c511b4869fea61ea52
Tobero <tobias.madlberger@gmail.com>
2022-03-30 13:51:26 +0200

    Added executeabled

--- src/index.js
+++ src/index.js
@@ -10,43 +10,38 @@ const MinecraftNet = require("./Minecraft");
 const config = YAML.load(fs.readFileSync("../config.yml"));
 
 (async () => {
-    console.log('[General]: Getting time delay for more accuracy...');
-
-    let delay;
-    try {
-        delay = await timeis.getDelay(config.country, config.delayDetection, !config.debugging);
-    } catch (e) {
-        console.log('[General]: Couldn\'t get delay. Stopping sniper');
-        return;
-    }
-    console.log(`[General]: Got your delay. Delay: ${delay}`);
-    console.log(`[General]: Trying to find minecraft username ${config.mcname} available time...`);
-
-    let time;
-    try {
-        time = await namemc.getAvailableTime(config.mcname, !config.debugging);
-    } catch (e) {
-        console.log(`[General]: Wasn't able to find time until name is usable. Stopping sniper`);
-        return;
-    }
-    const msTillFree = time - Date.now() + delay;
-
-    console.log(`[General]: Found name. Will rename account after ${msTillFree} ms`);
-
-    const Minecraft = new MinecraftNet();
-
-    setTimeout(async () => {
-        console.log(`[General]: Logging into Minecraft.net...`);
-        await Minecraft.open();
-        await Minecraft.login(config.minecraft-microsoft.email, config.minecraft-microsoft.password);
-        console.log(`[General]: Successfully logged into minecraft.net. Preparing new username`);
-        await Minecraft.changeName(config.mcname);
-    }, msTillFree - 60000);
-    setTimeout(async () => {
-        const success = await Minecraft.confirm();
-        console.log(`[General]: You ${success ? "did" : "didn't"} get the name`);
-        console.log("[General]: Stopping sniper...");
-        await Minecraft.close();
-    }, msTillFree);
+    createInterval();
 })();
 
+function createInterval() {
+    const interval = setInterval(async() => {
+        console.log("[Interval]: Getting timing...");
+        const delay = await timeis.getDelay(config.country, config.delayDetection, !config.debugging);
+        const time = await namemc.getAvailableTime(config.mcname, !config.debugging);
+        console.log(`[Interval]: Timings are: ${delay}ms delay, ${time - Date.now()}ms until free`);
+
+        const msTillFree = time - Date.now() + delay;
+        if (msTillFree <= 600000) {
+            console.log(`[Interval]: Final 10 minutes have entered. Preparing for accurate login`);
+            const Minecraft = new MinecraftNet();
+            setTimeout(() => {
+                login(Minecraft);
+            }, msTillFree - 60000);
+            setTimeout(async () => {
+                const success = await Minecraft.confirm();
+                console.log(`[General]: You ${success ? "did" : "didn't"} get the name`);
+                console.log("[General]: Stopping sniper...");
+            }, msTillFree);
+            clearInterval(interval);
+        }
+    }, 60 * 5000);
+    console.log("[General]: Started Sniper");
+}
+
+async function login(Minecraft) {
+    console.log(`[General]: Logging into Minecraft.net...`);
+    await Minecraft.open();
+    await Minecraft.login(config.minecraft-microsoft.email, config.minecraft-microsoft.password);
+    console.log(`[General]: Successfully logged into minecraft.net. Preparing new username`);
+    await Minecraft.changeName(config.mcname);
+}
